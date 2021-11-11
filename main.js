3551999760114ff41ad9bbb6e8df061ba06d91d3
Zachary Rowland <rowlandz1@udayton.edu>
2021-11-10 20:16:23 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,182 @@
+// Canvas API documentation
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+
+// Initialized by draw(), never altered.
+var ctx;
+var canvas;
+var intervals2pixels;
+
+var timespan = eventsInfo.end - eventsInfo.start;
+
+function draw() {
+  canvas = document.getElementById('canvas');
+  canvas.width = renderInfo.widthPx;
+  canvas.height = renderInfo.heightPx;
+  intervals2pixels = canvas.width / (eventsInfo.end - eventsInfo.start);
+
+  if (canvas.getContext == undefined) return;
+  ctx = canvas.getContext('2d');
+  ctx.lineWidth = 2;
+
+  // background tiling image
+  var bgimg = new Image();
+  bgimg.src = "scroll.jpg";
+  bgimg.onload = function() {
+    ctx.fillStyle = ctx.createPattern(bgimg, 'repeat');
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //ctx.drawImage(bgimg, 0, 0, canvas.width, canvas.height);
+    //ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+    //ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawMainLine(eventsInfo, canvas.height / 2);
+    drawIntervalEvents(eventsInfo.intervalEvents, canvas.height/2 + renderInfo.eventHeightPx/2 + 5, "align-left");
+    drawEvents(eventsInfo.events, canvas.height / 2 - 8, 50);
+  };
+
+  //var re117 = new Image();
+  // re117.src = 'RE117.png';
+  // re117.onload = function() {
+  //   ctx.drawImage(re117, 0, 150, 2534/2, 1614/2);
+  // };
+}
+
+function drawMainLine(line, yPx) {
+  var eraPxSize = line.eraSize * intervals2pixels;
+  var topPx = yPx - renderInfo.eventHeightPx/2;
+  var textYPx = topPx - 3;
+
+  var x = 0;
+  var timeLabel = line.start;
+  ctx.font = '14px Sans';
+
+  while (true) {
+    ctx.fillStyle = 'rgb(252, 224, 179)';
+    if(x + eraPxSize < canvas.width) {
+      ctx.fillRect(x, topPx, eraPxSize, renderInfo.eventHeightPx);
+      drawLabel(String(timeLabel), x+3, textYPx);
+      x += eraPxSize;
+      timeLabel += line.eraSize;
+    } else { break; }
+    ctx.fillStyle = 'rgb(209, 173, 116)';
+    if(x + eraPxSize < canvas.width) {
+      ctx.fillRect(x, topPx, eraPxSize, renderInfo.eventHeightPx);
+      drawLabel(String(timeLabel), x+3, textYPx);
+      x += eraPxSize;
+      timeLabel += line.eraSize;
+    } else { break; }
+  }
+  ctx.fillRect(x, topPx, canvas.width - x, renderInfo.eventHeightPx);
+  drawLabel(String(timeLabel), x+3, textYPx);
+}
+
+// Draw the interval events below the main line. The labelstyle determines how
+// to draw the labels:
+//   "squish"     -- Resize label to fit inside the interval
+//   "adj-right"  -- Draw label on the right, adjacent to the interval
+//   "align-left" -- No squish, just align left boundaries
+function drawIntervalEvents(evnts, yPx, labelstyle) {
+  if (evnts.length <= 0) return;
+  evnts.sort((x, y) => x.start - y.start);  // increasing start time
+  var lanes = [evnts[0].end*intervals2pixels]; // end times (in pixels)
+  var sepPx = renderInfo.eventHeightPx + renderInfo.eventSpacingPx;
+
+  drawIntervalEvent(evnts[0], yPx, labelstyle);
+  for (var i = 1; i < evnts.length; i++) {
+    var t = 0;
+    var evntStartPx = evnts[i].start * intervals2pixels;
+    var evntEndPx = evnts[i].end * intervals2pixels;
+    var evntTextWidth = ctx.measureText(evnts[i].name).width;
+
+    var rightBoundary;
+    if (labelstyle == "adj-right") {
+      rightBoundary = evntEndPx + evntTextWidth + 8;
+    } else if (labelstyle == "align-left") {
+      rightBoundary = Math.max(evntEndPx, evntStartPx + evntTextWidth + 8);
+    } else {
+      rightBoundary = evntEndPx;
+    }
+
+    while (t < lanes.length && evntStartPx < lanes[t]) t++;
+    drawIntervalEvent(evnts[i], yPx + sepPx*t, labelstyle);
+    if (t < lanes.length)
+      lanes[t] = rightBoundary;
+    else
+      lanes.push(rightBoundary);
+  }
+}
+
+function drawIntervalEvent(evnt, yPx, labelstyle) {
+  var evntPxStart = (evnt.start - eventsInfo.start) / timespan * canvas.width;
+  var evntPxLength = (evnt.end - evnt.start) / timespan * canvas.width;
+
+  // draw rounded rect
+  ctx.fillStyle = evnt.color;
+  fillRoundedRect(ctx, evntPxStart, yPx, evntPxLength, renderInfo.eventHeightPx, 5);
+  var oldstrokestyle = ctx.strokeStyle;
+  ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+  drawRoundedRect(ctx, evntPxStart, yPx, evntPxLength, renderInfo.eventHeightPx, 5);
+  ctx.strokeStyle = oldstrokestyle;
+
+  // draw text
+  if (labelstyle == "adj-right") {
+    drawLabel(evnt.name, evntPxStart + evntPxLength + 4, yPx + 16);
+  } else if (labelstyle == "align-left") {
+    drawLabel(evnt.name, evntPxStart + 4, yPx + 16);
+  } else {
+    ctx.fillStyle = 'white';
+    ctx.fillText(evnt.name, evntPxStart + 4, yPx + 16, evntPxLength - 8);
+  }
+}
+
+function drawEvents(evnts, pointYPx, stalkPx) {
+  if (evnts.length <= 0) return;
+  evnts.sort((a, b) => b.when - a.when);
+  var lanes = [];  // left pixel boundaries
+  var sepPx = 25;
+
+  var textWidth = ctx.measureText(evnts[0].name).width;
+  var pointXPx = (evnts[0].when - eventsInfo.start) / timespan * canvas.width;
+  var textXPx = Math.min(pointXPx - 10, canvas.width - textWidth);
+  lanes.push(textXPx);
+  drawEvent(evnts[0], pointXPx, pointYPx, textXPx, pointYPx - stalkPx, textWidth);
+  for (var i = 1; i < evnts.length; i++) {
+    textWidth = ctx.measureText(evnts[i].name).width;
+    pointXPx = (evnts[i].when - eventsInfo.start) / timespan * canvas.width;
+    textXPx = Math.min(pointXPx - 10, canvas.width - textWidth);
+
+    var t = 0;
+    while (t < lanes.length && lanes[t] < textWidth + textXPx) t++;
+    var textYPx = pointYPx - stalkPx - sepPx*t;
+    drawEvent(evnts[i], pointXPx, pointYPx, textXPx, textYPx, textWidth);
+    if (t < lanes.length)
+      lanes[t] = textXPx;
+    else
+      lanes.push(textXPx);
+  }
+}
+
+function drawEvent(evnt, pointXPx, pointYPx, textXPx, textYPx, textWidth) {
+  drawLabel(evnt.name, textXPx, textYPx, textWidth);
+
+  ctx.fillStyle = 'black';
+  ctx.strokeStyle = '#444444';
+  ctx.beginPath();
+  ctx.moveTo(pointXPx, pointYPx - 5);
+  ctx.lineTo(pointXPx, textYPx + 3);            // vertical line
+  ctx.moveTo(textXPx, textYPx + 3);
+  ctx.lineTo(textXPx + textWidth, textYPx + 3); // horizontal line
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(pointXPx, pointYPx, 5, 0, Math.PI * 2, true);
+  ctx.stroke();
+}
+
+// Draws a label with black text and a semi-transparent white background.
+// Providing a textWidth can prevent recalculating it.
+function drawLabel(text, x, y, textWidth=0) {
+  if (textWidth == 0) textWidth = ctx.measureText(text).width;
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
+  fillRoundedRect(ctx, x - 4, y - 14, textWidth + 8, 18, 9);
+  ctx.fillStyle = 'black';
+  ctx.fillText(text, x, y);
+}
---
+++ timeline.js
@@ -0,0 +1,460 @@
+var renderInfo = {
+  "widthPx"        : 6500,
+  "heightPx"       : 800,
+  "eventHeightPx"  : 30,
+  "eventSpacingPx" : 5
+};
+
+var eventsInfo = {
+  "start"   : -800,
+  "end"     : 500,
+  "eraSize" : 100,
+  "events"  : [
+    {
+      "name" : "Founding of Rome",
+      "when" : -753
+    },
+    {
+      "name" : "Founding of the Republic",
+      "when" : -509
+    },
+    {
+      "name" : "Twelve Tables",
+      "when" : -451
+    },
+    {
+      "name" : "Rome Sacked by Gauls",
+      "when" : -390
+    },
+    {
+      "name" : "Battle of Cannae",
+      "when" : -216
+    },
+    {
+      "name" : "Battle of Zama",
+      "when" : -202
+    },
+    {
+      "name" : "Catiline Conspiracy",
+      "when" : -63
+    },
+    {
+      "name" : "Crossing of the Rubicon",
+      "when" : -49
+    },
+    {
+      "name" : "Julius Caesar Killed",
+      "when" : -44
+    },
+    {
+      "name" : "Battle of Actium",
+      "when" : -2
+    },
+    {
+      "name" : "Battle of the Teutoburg Forest",
+      "when" : 9
+    },
+    {
+      "name" : "Crucifixion of Jesus",
+      "when" : 33
+    },
+    {
+      "name" : "Rome Invades Britain",
+      "when" : 43
+    },
+    {
+      "name" : "Boudican Revolt",
+      "when" : 60
+    },
+    {
+      "name" : "Great Fire of Rome",
+      "when" : 64
+    },
+    {
+      "name" : "Year of Four Emperors",
+      "when" : 69
+    },
+    {
+      "name" : "Colosseum Completed",
+      "when" : 80
+    },
+    {
+      "name" : "Hadrian's Wall Completed",
+      "when" : 128
+    },
+    {
+      "name" : "Year of Five Emperors",
+      "when" : 193
+    },
+    {
+      "name" : "Sassanid Empire",
+      "when" : 224
+    },
+    {
+      "name" : "Year of Six Emperors",
+      "when" : 238
+    },
+    {
+      "name" : "Death of Mani",
+      "when" : 277
+    },
+    {
+      "name" : "Battle of the Milvian Bridge",
+      "when" : 312
+    },
+    {
+      "name" : "Edict of Milan",
+      "when" : 313
+    },
+    {
+      "name" : "Founding of Constantinople",
+      "when" : 330
+    },
+    {
+      "name" : "Great Conspiracy",
+      "when" : 368
+    },
+    {
+      "name" : "Massacre of Thessalonica",
+      "when" : 390
+    },
+    {
+      "name" : "Ravenna Becomes Western Capital",
+      "when" : 402
+    },
+    {
+      "name" : "Barbarian Invasion",
+      "when" : 406
+    },
+    {
+      "name" : "Sack of Rome",
+      "when" : 410
+    },
+    {
+      "name" : "Vandal Kingdom Established",
+      "when" : 439
+    },
+    {
+      "name" : "Vandal Sack of Rome",
+      "when" : 455
+    },
+    {
+      "name" : "Augustulus Deposed",
+      "when" : 476
+    }
+  ],
+  "intervalEvents"  : [
+    {
+      "name"  : "Romulus",
+      "start" : -753,
+      "end"   : -715,
+      "color" : "gold"
+    },
+    {
+      "name"  : "Numa Pompilius",
+      "start" : -715,
+      "end"   : -673,
+      "color" : "gold"
+    },
+    {
+      "name"  : "Tullus Hostilius",
+      "start" : -673,
+      "end"   : -642,
+      "color" : "gold"
+    },
+    {
+      "name"  : "Ancus Martius",
+      "start" : -642,
+      "end"   : -616,
+      "color" : "gold"
+    },
+    {
+      "name"  : "Tarquinius Priscus",
+      "start" : -616,
+      "end"   : -579,
+      "color" : "gold"
+    },
+    {
+      "name"  : "Servius Tullius",
+      "start" : -579,
+      "end"   : -534,
+      "color" : "gold"
+    },
+    {
+      "name"  : "Tarquinius Superbus",
+      "start" : -534,
+      "end"   : -509,
+      "color" : "gold"
+    },
+    {
+      "name"  : "First Samnite War",
+      "start" : -343,
+      "end"   : -341,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Latin War",
+      "start" : -340,
+      "end"   : -338,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Alexander the Great",
+      "start" : -336,
+      "end"   : -323,
+      "color" : "blue"
+    },
+    {
+      "name"  : "Second Samnite War",
+      "start" : -326,
+      "end"   : -304,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Third Samnite War",
+      "start" : -298,
+      "end"   : -290,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Pyrrhic War",
+      "start" : -280,
+      "end"   : -275,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "First Punic War",
+      "start" : -264,
+      "end"   : -241,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Second Punic War",
+      "start" : -218,
+      "end"   : -201,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Second Macedonian War",
+      "start" : -200,
+      "end"   : -197,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Third Macedonian War",
+      "start" : -171,
+      "end"   : -168,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Third Punic War",
+      "start" : -149,
+      "end"   : -146,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Social War",
+      "start" : -91,
+      "end"   : -87,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "First Mithridatic War",
+      "start" : -89,
+      "end"   : -85,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Third Servile War",
+      "start" : -73,
+      "end"   : -71,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Gallic Wars",
+      "start" : -58,
+      "end"   : -50,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Augustus",
+      "start" : -27,
+      "end"   : 14,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Tiberius",
+      "start" : 14,
+      "end"   : 37,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Caligula",
+      "start" : 37,
+      "end"   : 41,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Claudius",
+      "start" : 41,
+      "end"   : 54,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Nero",
+      "start" : 54,
+      "end"   : 68,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Great Jewish Revolt",
+      "start" : 66,
+      "end"   : 73,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Vespasian",
+      "start" : 69,
+      "end"   : 79,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Domitian",
+      "start" : 81,
+      "end"   : 96,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Nerva",
+      "start" : 96,
+      "end"   : 98,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Trajan",
+      "start" : 98,
+      "end"   : 117,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Hadrian",
+      "start" : 117,
+      "end"   : 138,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Antoninus Pius",
+      "start" : 138,
+      "end"   : 161,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Marcus Aurelius",
+      "start" : 161,
+      "end"   : 180,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Marcomannic Wars",
+      "start" : 166,
+      "end"   : 180,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Commodus",
+      "start" : 176,
+      "end"   : 192,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Antonine Plague",
+      "start" : 165,
+      "end"   : 180,
+      "color" : "green"
+    },
+    {
+      "name"  : "Severus",
+      "start" : 193,
+      "end"   : 211,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Crisis of the Third Century",
+      "start" : 235,
+      "end"   : 284,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Cyprian Plague",
+      "start" : 249,
+      "end"   : 262,
+      "color" : "green"
+    },
+    {
+      "name"  : "Valerian",
+      "start" : 253,
+      "end"   : 260,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Aurelian",
+      "start" : 270,
+      "end"   : 275,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Diocletian",
+      "start" : 284,
+      "end"   : 305,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Constantine",
+      "start" : 306,
+      "end"   : 337,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Gothic War",
+      "start" : 376,
+      "end"   : 382,
+      "color" : "#EE1111"
+    },
+    {
+      "name"  : "Theodosius",
+      "start" : 379,
+      "end"   : 395,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Stilicho",
+      "start" : 382,
+      "end"   : 408,
+      "color" : "orange"
+    },
+    {
+      "name"  : "Honorius",
+      "start" : 393,
+      "end"   : 423,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Valentinian III",
+      "start" : 425,
+      "end"   : 455,
+      "color" : "#7851A9"
+    },
+    {
+      "name"  : "Aëtius",
+      "start" : 433,
+      "end"   : 454,
+      "color" : "orange"
+    },
+    {
+      "name"  : "Ricimer",
+      "start" : 461,
+      "end"   : 472,
+      "color" : "orange"
+    }
+  ]
+};
